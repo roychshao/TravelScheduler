74e6b120a59d2d87b0bd06469091ad21b4ef2f3f
roychshao <roych.shao@gmail.com>
2023-05-01 14:28:28 +0800

    feat: logout btn

--- view/src/pages/Catalog/User/User.jsx
+++ view/src/pages/Catalog/User/User.jsx
@@ -1,6 +1,9 @@
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { useNavigate } from 'react-router-dom'
+import { getAuth, signOut } from 'firebase/auth'
+import { loggedoutwithgoogle } from './../../../actions/loginAction.js'
+import Button from '@mui/material/Button';
 
 const User = () => {
    
@@ -11,11 +14,26 @@ const User = () => {
     const email = useSelector(state => state.loginReducer.email);
     const photoURL = useSelector(state => state.loginReducer.photoURL);
     
+    const handleLogout = () => {
+        // 確認是在登入狀態
+        if(displayName && email && photoURL) {
+            dispatcher(loggedoutwithgoogle());
+            const auth = getAuth();
+            signOut(auth).then(() => {
+                console.log("successfully logout");
+                navigate('/');
+            }).catch((err) => {
+                console.log("err: " + err.message);
+            })
+        }
+    }
+
     return (
         <div>
             <img src={photoURL} alt="user photo"/>
             <p>{displayName}</p>
             <p>{email}</p>
+            <Button variant="contained" onClick={handleLogout}>logout</Button>
         </div>
     )
 }
