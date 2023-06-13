366fa2269b043f9bbd5685f487edfa94ad4457ed
vincent0318 <vincent0318@gmail.com>
2023-06-13 14:35:48 +0800

    update travel "get1" get user_id from session

--- backend/controller/travel.js
+++ backend/controller/travel.js
@@ -86,36 +86,32 @@ export const edit = async (req, res, next) => {
 
 
 export const get1= async (req, res, next) => {
-   const {
-     user_id,
-   } = req.body;
-   await Travel.get1(user_id)
-     .then((result) => {
-       console.log(result);
-       var data = {
-         travels: [],
-       };
-       for( let i = 0; i < result[0].length; ++i) {
-         var travel = {
-            travel_id: parseEscape(result[0][i].travel_id),
-            group_id: parseEscape(result[0][i].group_id),
-            travel_name: parseEscape(result[0][i].name),
-            travel_date: parseEscape(result[0][i].date),
-            travel_peoplenum: parseEscape(result[0][i].people_num),
-            travel_description: parseEscape(result[0][i].description),
-            travel_done: parseEscape(result[0][i].done)
-         };
-         data.travels.push(travel);
-       }
-       req.data = JSON.stringify(data);
-       next();
-     })
-     .catch((err) => {
-       req.err = err;
-       next();
-     });
-
-     
+  const { user_id } = req?.session;
+  await Travel.get1(user_id)
+    .then((result) => {
+      console.log(result);
+      var data = {
+        travels: [],
+      };
+      for (let i = 0; i < result[0].length; ++i) {
+        var travel = {
+          travel_id: parseEscape(result[0][i].travel_id),
+          group_id: parseEscape(result[0][i].group_id),
+          travel_name: parseEscape(result[0][i].name),
+          travel_date: parseEscape(result[0][i].date),
+          travel_peoplenum: parseEscape(result[0][i].people_num),
+          travel_description: parseEscape(result[0][i].description),
+          travel_done: parseEscape(result[0][i].done),
+        };
+        data.travels.push(travel);
+      }
+      req.data = JSON.stringify(data);
+      next();
+    })
+    .catch((err) => {
+      req.err = err;
+      next();
+    });
 };
 
 export const get2 = async (req, res, next) => {
