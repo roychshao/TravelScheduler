221f705066294fff0a70f020bda8e3e48759d319
109703052 <qsd754896123@gmail.com>
2023-06-20 04:35:55 +0800

    TravelList.jsx

--- backend/database/travel.js
+++ backend/database/travel.js
@@ -115,8 +115,8 @@ const edit = (
       "UPDATE `TRAVEL` SET group_id = ?, name = ?, date = ?, people_num = ?, description = ?, done = ? WHERE travel_id = ?",
     ];
 
-    if (group_id) 
-      user_id = null;
+    // if (group_id) 
+    //   user_id = null;
 
     var values = [
       [
